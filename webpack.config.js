d84ebbf93db0b4f71d564fe7030052e61486f727
Artem Rybakov <gizmo3000@yandex.ru>
2020-06-23 10:42:03 +0300

    add babel loader support

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,20 @@ const ClearPlugin = require('clean-webpack-plugin');
 module.exports = {
   entry: './src/app.js',
   mode: 'development',
+  module: {
+    rules: [
+      {
+        exclude: /(node_modules|bower_components)/,
+        test: /\.m?js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'assets', 'scripts'),
